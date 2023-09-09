83c163b13da5acaed1e3aa8f34fceb4bd0b9bd3f
Progress Okorugbo <progressokorugbo@gmail.com>
2023-09-08 17:56:11 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - program that prints tha alphabet in lowercase and uppercase
+*Return: always 0
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	putchar('\n');
+	return (0);
+}
