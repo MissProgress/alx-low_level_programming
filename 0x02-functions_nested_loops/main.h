fbc8e789d4aebeaaa82d8ff98c3b78601a5deb7c
Progress Okorugbo <progressokorugbo@gmail.com>
2023-09-12 15:32:12 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int n);
+void print_times_table(int n);
+void jack_bauer(void);
+int print_last_digit(int);
+int _abs(int);
+int print_sign(int n);
+int _isalpha(int c);
+int _islower(int c);
+
+#endif
