0192d300e9c7c9f04e466f4aaed2cf6536cdd5bb
Progress Okorugbo <progressokorugbo@gmail.com>
2023-09-14 15:55:49 -0700

    2-m

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - multiplies two integers
+* @a: function parameter a
+* @b: function parameter b
+* Return: a, b
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
