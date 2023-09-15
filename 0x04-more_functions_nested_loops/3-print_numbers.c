12b83049e6d377b1b733ef312c3010f601a8ae5d
Progress Okorugbo <progressokorugbo@gmail.com>
2023-09-14 17:18:39 -0700

    9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - prints the numbers, from 0 to 9, followed by a new line.
+* Return: void
+*/
+
+void print_numbers(void)
+{
+char m;
+
+for (m = '0'; m < '9'; m++)
+{
+	_putchar(m);
+}
+	_putchar('\n');
+}
