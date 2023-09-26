039ce2c95d935bb9314950021ca89406f2e01a46
Progress Okorugbo <progressokorugbo@gmail.com>
2023-09-26 09:21:53 -0700

    task 6 solution

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_line - draws a straight line in the terminal
+* @n: length of line
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
+}
