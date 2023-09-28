db8005a7860d470e1019e270199ed6dfc6273da4
Progress Okorugbo <progressokorugbo@gmail.com>
2023-09-28 14:26:24 -0700

    Task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* is_prime_number - function name
+* @n: function parameter
+* Return: 1 if prime number, otherwise 0
+*/
+int is_prime_number(int n)
+{
+
+	int i;
+	if (n <= 1)
+		return (0);
+	for (i = 2; i * i  <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int is_prime_number(int n);
+
+#endif
