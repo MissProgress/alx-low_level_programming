0a8a0b1e01c70b04b27099fee3984666ff35dee3
Progress Okorugbo <progressokorugbo@gmail.com>
2023-10-03 01:02:26 +0100

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments
+ * @argc: number of command line arg
+ * @argv: aray that contains the program command line arg
+ * Return: 0 on success
+ */
+
+int main(int argc, chaar *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
