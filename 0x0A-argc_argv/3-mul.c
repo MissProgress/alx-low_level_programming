07733c32803889ae71c2558b2c71b322aaac77ec
Progress Okorugbo <progressokorugbo@gmail.com>
2023-10-02 17:15:35 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * @argc: number of command line arg
+ * @argv: aray that contains the program command line arg
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
