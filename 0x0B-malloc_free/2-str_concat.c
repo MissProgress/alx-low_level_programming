9ffc8ce160738224e503cd16174ea5a606aaa13b
Progress Okorugbo <progressokorugbo@gmail.com>
2023-10-04 15:22:23 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,16 +14,24 @@ char *str_concat(char *s1, char *s2)
 	int i, c;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
 
 	i = c = 0;
 	while (s1[i] != '\0')
+	{
 		i++;
+	}
 	while (s2[c] != '\0')
+	{
 		c++;
-	conc = malloc(sizeof(char) * (i = c + 1));
+	}
+	conc = malloc(sizeof(char) * (i + c + 1));
 
 	if (conc == NULL)
 		return (NULL);
