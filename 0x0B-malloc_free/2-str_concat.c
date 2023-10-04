407443bda6acad48c873a93d36fba0b3cf828d26
Progress Okorugbo <progressokorugbo@gmail.com>
2023-10-04 16:03:38 -0700

    solutions

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,43 +10,36 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *conc;
-	int i, c;
+	char *conct;
+	int i, ci;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
-	i = c = 0;
+	i = ci = 0;
 	while (s1[i] != '\0')
-	{
 		i++;
-	}
-	while (s2[c] != '\0')
-	{
-		c++;
-	}
-	conc = malloc(sizeof(char) * (i + c + 1));
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (i + ci + 1));
 
-	if (conc == NULL)
+	if (conct == NULL)
 		return (NULL);
-	i = c = 0;
+	i = ci = 0;
 	while (s1[i] != '\0')
 	{
-		conc[i] = s1[i];
+		conct[i] = s1[i];
 		i++;
 	}
 
-	while (s2[2] != '\0')
+	while (s2[ci] != '\0')
 	{
-		conc[i] = s2[c];
-	i++, c++;
+		conct[i] = s2[ci];
+		i++, ci++;
 	}
-	conc[i] = '\0';
-	return (conc);
+	conct[i] = '\0';
+	return (conct);
 }
+
