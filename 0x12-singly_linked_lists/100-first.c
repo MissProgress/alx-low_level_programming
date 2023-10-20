8ddfea0071e359b8f15c10df4c921ca39bc158e4
MissProgress <progressokorugbo@gmail.com>
2023-10-20 03:08:56 +0100

    ádvanced,

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+void come_first(void)__attribute__((constructor));
+/**
+ * come_first - function runs before the main function
+ */
+void come_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
