14dd7163466480fdd80812a048d20b457cd178c0
MissProgress <progressokorugbo@gmail.com>
2023-11-04 20:38:52 +0100

    Task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars n init with a specific char
+ * @size: amount of space to be allocated
+ * @c: character to be initialized
+ * Return: pointer to the first member
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptArr;
+
+	if (size == 0)
+		return (NULL);
+
+	ptArr = malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+		ptArr = c;
+
+	return (ptArray);
+}
