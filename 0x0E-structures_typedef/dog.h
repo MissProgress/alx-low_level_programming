243a3fa52d7fda95a5a10b30a0e29496e9ac85ca
MissProgress <progressokorugbo@gmail.com>
2023-11-07 12:16:31 -0800

    solution

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - name of new struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
