28206abb4e03953519221c15e9bbdd078af38789
MissProgress <progressokorugbo@gmail.com>
2023-11-07 13:15:51 -0800

    checked

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,13 +9,13 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		if (!(d->name))
-			printf("Name: (nil)");
+			printf("Name: (nil)\n");
 		else
-			printf("Name: %s", (*d).name);
-		printf("Age: %f", (*d).age);
+			printf("Name: %s\n", (*d).name);
+		printf("Age: %f\n", (*d).age);
 		if (!(d->owner))
-			printf("Owner: (nil)");
+			printf("Owner: (nil)\n");
 		else
-			printf("Owner: %s", (*d).owner);
+			printf("Owner: %s\n", (*d).owner);
 	}
 }
