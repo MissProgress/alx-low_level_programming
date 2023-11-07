4089b8b78917a45860ef7fe7f2780e724fce4052
MissProgress <progressokorugbo@gmail.com>
2023-11-07 12:21:22 -0800

    task1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - initializing th struct dog
+ * @d: pointer to the new variable name
+ * @name: Pointer to the new name
+ * @age: Pointer to the new age
+ * @owner: pointer to the new dog owner
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
