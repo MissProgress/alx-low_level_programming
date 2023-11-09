a93e590f3e87fe27a3da5d850cb397bb136cb2a4
MissProgress <progressokorugbo@gmail.com>
2023-11-09 13:44:33 -0800

    task0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,6 +1,26 @@
+#include <stdarg.h>
 #include "variadic_functions.h"
 
 int sum_them_all(const unsigned int n, ...)
 {
-	
+	unsigned int sum = 0;
+	unsigned int i;
+
+	va_list hold;
+
+	va_start(hold, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (n == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			sum += va_arg(hold, const unsigned int);
+		}
+	}
+	va_end(hold);
+	return (sum);
 }
